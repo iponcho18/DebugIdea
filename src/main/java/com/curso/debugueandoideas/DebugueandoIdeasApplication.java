644932d793c07b12e2b138ce1edaa96a20bfb58b
Jose Quezada <jaqibarra.sistemas@gmail.com>
2025-12-30 18:25:09 -0600

    LOG: Se agrega la configuracion basica del proyecto

---
+++ src/main/java/com/curso/debugueandoideas/DebugueandoIdeasApplication.java
@@ -0,0 +1,13 @@
+package com.curso.debugueandoideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugueandoIdeasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebugueandoIdeasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/debugueandoideas/DebugueandoIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.debugueandoideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugueandoIdeasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
